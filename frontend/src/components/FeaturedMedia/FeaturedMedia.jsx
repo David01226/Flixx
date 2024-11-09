09569869f6a36ba6b88543bf6728a4fdd93fc8e6
David Gibson <david@dream-big-digital.co.uk>
2024-11-09 14:29:29 +0000

    build featured media meta section and pull through movie backdrop image

--- frontend/src/components/FeaturedMedia/FeaturedMedia.jsx
+++ frontend/src/components/FeaturedMedia/FeaturedMedia.jsx
@@ -10,9 +10,9 @@ const FeaturedMedia = ({ results }) => {
   // Set movieId when results change
   useEffect(() => {
     if (results && results.length > 0) {
-      // console.log("Movie ID:", results[0].id);
-      setMovie(results[0]);
-      setMovieId(results[0].id);
+      // console.log("Movie ID:", results[1].id);
+      setMovie(results[1]);
+      setMovieId(results[1].id);
     }
   }, [results]);
 
@@ -38,6 +38,8 @@ const FeaturedMedia = ({ results }) => {
     fetchVideoData();
   }, [movieId]);
 
+  console.log(movie)
+
   return (
     <div className="featured-media">
       <div className="featured-media__media-wrap">
@@ -51,8 +53,27 @@ const FeaturedMedia = ({ results }) => {
             allowFullScreen
           ></iframe>
         )}
+        {movie && (
+          <>
+            {/* <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title} /> */}
+            <img className="featured-media__backdrop-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+            <div className="featured-media__meta">
+              <h2>{movie.title}</h2>
+              <div className="featured-media__description">{movie.overview}</div>
+              <div className="featured-media__ctas">
+                <a className="btn play-btn" href="/">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="PlayStandard" aria-hidden="true"><path d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z" fill="currentColor"></path></svg>
+                  <div>Play</div>
+                </a>
+                <a className="btn more-info-btn" href="/">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="CircleIStandard" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg>
+                  <div>More Info</div>
+                </a>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      {/* {movie && <div>{movie.title}</div>} */}
     </div>
   );
 };
